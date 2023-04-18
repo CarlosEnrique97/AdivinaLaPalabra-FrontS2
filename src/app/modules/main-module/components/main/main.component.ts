e3096b1734cb434d85d2013dfd73e7b39aee9cbb
PERALTA Carlos Enrique <ce.garcia@ext.soprasteria.com>
2023-04-18 13:13:45 +0200

    Teclado

--- src/app/modules/main-module/components/main/main.component.ts
+++ src/app/modules/main-module/components/main/main.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  
+  Teclado: string[] = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'Ñ',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
 
+  EnviarLetra(Tecla: string){
+    console.log(Tecla);
+  }
+  
 }
