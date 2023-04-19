79f564f3f2c9e85281f30cc1770a8ed74cb3a53b
AlejandroPallardo <aj.benavent@ext.soprasteria.com>
2023-04-19 09:26:57 +0200

    D

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -10,31 +10,12 @@ export class GameService {
   baseURL = 'http://10.102.31.7:8080/';
   constructor(private http: HttpClient) {}
 
-<<<<<<< HEAD
-  wordExist: any;
-  BASEURL = 'http://10.102.31.7:8080/';
-  constructor(private http : HttpClient) {}
-
   getInfoWord(word: string){
-    this.http.get(this.BASEURL.concat(`checkIfWordExists/${word}`)).subscribe({
+    this.http.get(this.baseURL.concat(`checkIfWordExists/${word}`)).subscribe({
       next: (response) => {
         if(!response) return;
         this.wordExist = response;
       }
     })
   }
-
-=======
-  getWordIfExist(wordInsert: string) {
-    this.http
-      .get<any>(this.baseURL.concat(`checkIfWordExists/${wordInsert}`))
-      .subscribe({
-        next: (response) => {
-          if (!response) return;
-          this.wordExist = response;
-        },
-      });
-    console.log(this.wordExist);
-  }
->>>>>>> d87256c6cf92c8cd4ebf59312a19c36b033a6b6a
 }
