8122155f47b705bfac0e3deeb7b1d5638cc812ac
AlejandroPallardo <aj.benavent@ext.soprasteria.com>
2023-04-21 11:13:52 +0200

    Fase1

---
+++ src/app/interceptores/main.interceptor.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { DialogComponent } from '../components/dialog/dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
+
+@Injectable()
+export class MainInterceptor implements HttpInterceptor {
+
+
+constructor(private dialog: MatDialog){}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error) => {
+        console.log(error)
+        let data = ""
+        if(error.url === "http://10.102.31.7:8080/newGame"){
+          data = "Ha habido un fallo al generar la partida, ya se ve lo looser que eres, recarga anda"
+        }else if (error.url === "http://10.102.31.7:8080/checkIfWordExists"){
+          data="bbbbb"
+        }
+        return throwError(
+         this.dialog.open(DialogComponent, {
+          data: data,
+        }))
+      }));
+  }
+
+}
--- src/app/interfaces/palabra.ts
+++ src/app/interfaces/palabra.ts
@@ -1,3 +1,7 @@
 export type Palabra = {
   nombre: string;
 };
+
+export interface NewGame{
+  game_id: number;
+}
