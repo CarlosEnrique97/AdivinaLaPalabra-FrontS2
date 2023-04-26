02ac201f9324bc5307c9e85906b13501c043d0a8
AlejandroPallardo <aj.benavent@ext.soprasteria.com>
2023-04-26 11:13:17 +0200

    refact(Interceptor)

--- src/app/interceptores/main.interceptor.ts
+++ src/app/interceptores/main.interceptor.ts
@@ -10,38 +10,44 @@ import { DialogComponent } from '../components/dialog/dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { GameService } from '../services/game.service';
 
-
 @Injectable()
 export class MainInterceptor implements HttpInterceptor {
-  
+  constructor(private dialog: MatDialog, private gameService: GameService) {}
 
-
-constructor(private dialog: MatDialog, private gameService: GameService){}
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((error) => {
-        let errorMessage = '';
-        console.log(error);
-        if (error.status === 0) {
-          errorMessage = 'UPS, algo ha ido mal.';
-        } else if (error.status === 500 && error.url === "http://10.102.31.7:8080/newGame") {
-          errorMessage =
-            'Ha habido un fallo al generar la partida, ya se ve lo looser que eres, recarga anda';
-        }else if(error.status === 500 && error.url === "http://10.102.31.7:8080/checkIfWordExists/"+this.gameService.id.toString()){
-          errorMessage = 'UPS, algo ha ido mal, no podremos saber lo looser que eres...';
-        }
-        return throwError(
-         this.dialogError(errorMessage)
-        );
+        return throwError(this.MessageError(error));
       })
     );
   }
 
-  dialogError(error: string) {
+  MessageError(error: any){
+    let errorMessage = '';
+    
+    if (
+      error.status === 500 &&
+      error.url === 'http://10.102.31.7:8080/newGame'
+    ) {
+      errorMessage =
+        'Ha habido un fallo al generar la partida, ya se ve lo looser que eres, recarga anda';
+    } else if (
+      error.status === 500 &&
+      error.url ===
+        'http://10.102.31.7:8080/checkIfWordExists/' +
+          this.gameService.id.toString()
+    ) {
+      errorMessage =
+        'UPS, algo ha ido mal, no podremos saber lo looser que eres...';
+    }
+    errorMessage = 'UPS, algo ha ido mal.';
+
     this.dialog.open(DialogComponent, {
       data: {
-        text: error,
+        text: errorMessage,
         createButton: false,
       },
     });
