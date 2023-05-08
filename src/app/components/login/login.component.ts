c82474c2fbb19f2d72ec97d6d0363ff5b670d7d3
CarlosEnrique97 <ce.garcia@ext.soprasteria.com>
2023-05-08 11:25:44 +0200

    feat(servicio_login)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,36 +1,47 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent{
-
+export class LoginComponent {
   userForm: FormGroup = this.formBuilder.group({
-    username:[null,[Validators.required]],
-    password:[null,[Validators.required]]
+    username: [null, [Validators.required]],
+    password: [null, [Validators.required]],
   });
 
-  formValue:any;
+  formValue: any;
 
-  constructor(private formBuilder: FormBuilder, public router: Router){}
+  constructor(
+    private authservice: AuthService,
+    private formBuilder: FormBuilder,
+    public router: Router
+  ) {}
 
+  login() {
+    const username = this.userForm.value.username;
+    let bytesusername = new TextEncoder().encode(username);
+    let usernameEncrypt = btoa(
+      String.fromCharCode(...new Uint8Array(bytesusername))
+    );
 
-  login(){
+    const password = this.userForm.value.password;
+    let bytespassword = new TextEncoder().encode(password);
+    let passwordEncrypt = btoa(
+      String.fromCharCode(...new Uint8Array(bytespassword))
+    );
 
-    const username = this.userForm.value.username
-    let bytesusername= new TextEncoder().encode(username)
-    let usernameEncrypt = btoa(String.fromCharCode(...new Uint8Array(bytesusername)));
-    
-    const password = this.userForm.value.password
-    let bytespassword= new TextEncoder().encode(password)
-    let passwordEncrypt = btoa(String.fromCharCode(...new Uint8Array(bytespassword)));
-    
-    const user= { name: usernameEncrypt, password: passwordEncrypt};
+    const user = { name: usernameEncrypt, password: passwordEncrypt };
 
+    this.authservice.login(user).subscribe({
+      next: (response: any) => {
+        const token = response.token;
+        this.router.navigateByUrl('/');
+      },
+    });
   }
-
 }
