d03b673ebdc2acdb90d547a2b3c9528e7338e4f7
CarlosEnrique97 <ce.garcia@ext.soprasteria.com>
2023-05-09 11:16:48 +0200

    feat(interceptor)

--- src/app/interceptores/login.interceptor.ts
+++ src/app/interceptores/login.interceptor.ts
@@ -7,27 +7,27 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { StorageService } from '../services/storage.service';
 
 @Injectable()
 export class Logininterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private storageService: StorageService) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const token =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiY0dGMVptRnVaRzl6IiwiaWF0IjoxNjgzMjgwNTk2LCJleHAiOjE3NzMyODA1OTZ9.j4zZFf1aC63GxV4QUizbZWQQbLh8Ve0gc1CA7Bse7K0';
+    const token = this.storageService.getToken();
 
     if (token != null) {
       const TokenReq = req.clone({
         setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
       return next.handle(TokenReq);
     } else {
-      this.router.navigate(['login']);
+      this.router.navigate(['/login']);
       return next.handle(req);
     }
   }
