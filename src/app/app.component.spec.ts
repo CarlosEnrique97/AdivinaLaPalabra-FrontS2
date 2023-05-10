3ebd0f2ac55e66407b27011a59e5185e66b35841
CarlosEnrique97 <ce.garcia@ext.soprasteria.com>
2023-05-10 16:57:14 +0200

    refact(tests)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,52 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { DialogFinishComponent } from './components/dialog-finish/dialog-finish.component';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { GameHistoricModuleModule } from './modules/game-historic-module/game-historic-module.module';
+import { MainModuleModule } from './modules/main-module/main-module.module';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+      imports: [BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        MainModuleModule,
+        BrowserAnimationsModule,
+        MatDialogModule,
+        FormsModule,
+        ReactiveFormsModule,
+        GameHistoricModuleModule],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
+        FooterComponent,
+        DialogComponent,
+        DialogFinishComponent,
+        LoginComponent,
       ],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('Se deberia crear', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AdivinaLaPalabra-Front'`, () => {
+  it(`Deberia tener el titulo de 'AdivinaLaPalabra-Front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('AdivinaLaPalabra-Front');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AdivinaLaPalabra-Front app is running!');
-  });
 });
