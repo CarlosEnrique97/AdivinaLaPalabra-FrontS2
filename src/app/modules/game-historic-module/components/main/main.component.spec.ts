ff24ee4e57f0e92e75e470475e6114d3cdc814a6
CarlosEnrique97 <ce.garcia@ext.soprasteria.com>
2023-05-10 17:03:34 +0200

    refact(tests)

--- src/app/modules/game-historic-module/components/main/main.component.spec.ts
+++ src/app/modules/game-historic-module/components/main/main.component.spec.ts
@@ -2,22 +2,61 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainComponent } from './main.component';
 
-describe('MainComponent', () => {
+import { CommonModule, DatePipe } from '@angular/common';
+
+import { GameHistoricModuleRoutingModule } from '../../game-historic-module-routing.module';
+
+import { LastTenGames } from 'src/app/interfaces/palabra';
+
+import { HttpClientModule } from '@angular/common/http';
+
+fdescribe('MainComponent', () => {
   let component: MainComponent;
+
   let fixture: ComponentFixture<MainComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
-    })
-    .compileComponents();
+      imports: [
+        CommonModule,
 
-    fixture = TestBed.createComponent(MainComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+        GameHistoricModuleRoutingModule,
+
+        HttpClientModule,
+      ],
+
+      declarations: [MainComponent],
+    })
+
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(MainComponent);
+
+        component = fixture.componentInstance;
+
+        fixture.detectChanges();
+      });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Debe convertir una fecha de tipo String a un Date', () => {
+    const tenGame: LastTenGames = {
+      date: '10-09-2001 08:00',
+
+      winned: false,
+
+      attempts: 0,
+    };
+
+    const lastTenGames: LastTenGames[] = component.lastTenGames;
+
+    lastTenGames.push(tenGame);
+
+    component.convertDate();
+
+    expect(lastTenGames[0].date).toEqual('09/10/2001 08:00');
   });
 });
